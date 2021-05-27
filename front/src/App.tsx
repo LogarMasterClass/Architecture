8953f05c0a878ee383033a763d3acd0e499b2a9c
GIVAILSON NEVES (CRGL-THIRDPARTY.COM) <GIVAILSON_NEVES@CRGL-THIRDPARTY.COM>
2021-05-27 08:47:28 -0300

    server horroble

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,104 @@
+class SingleTom {
+  private counter: number = 1;
+  private static instace: SingleTom;
+
+  private constructor() {}
+
+  static getInstance(): SingleTom {
+    if (!SingleTom.instace) {
+      SingleTom.instace = new SingleTom();
+    }
+    return SingleTom.instace;
+  }
+
+  helloWord() {
+    console.log(`olá mundo!` + this.counter);
+    this.counter++;
+  }
+}
+
+// factory
+interface IPagamento {
+  pagar(): string;
+}
+
+class PagamentoEmCartao implements IPagamento {
+  pagar(): string {
+    return this.pagarEmCartao();
+  }  
+  pagarEmCartao() {
+    return 'toma cartão';
+  }
+}
+
+class PagamentoEmDinheiro implements IPagamento {
+  pagar(): string {
+    return "toma dinheiro";
+  }  
+}
+
+class PagamentoComPix implements IPagamento {
+  pagar(): string {
+    return "toma pix";
+  }  
+}
+
+class pagamentoFactory {
+  private constructor() {};
+
+  static factory(tipoPagamento?: string): IPagamento {
+    if (tipoPagamento) {
+      switch(tipoPagamento) {
+        case 'dinheiro':
+          return new PagamentoEmDinheiro();
+        case 'cartao':
+          return new PagamentoEmCartao();
+        case 'pix':
+          return new PagamentoComPix();
+      }
+    }
+    return new PagamentoEmDinheiro();
+  }
+}
+
+interface Passaro {
+  bica(): void;  
+}
+
+interface PassaroQueVoam extends Passaro {
+  voa(): void;  
+}
+
+class Pardao implements PassaroQueVoam {
+  bica(): void {}
+  voa(): void {}
+}
+
+class Pinguim implements Passaro {
+  bica(): void {}
+}
+
+
 function App() {
+
+  function olaMundo() {
+    let a = SingleTom.getInstance();
+    let b = SingleTom.getInstance();
+    let c = SingleTom.getInstance();
+    a.helloWord();
+    b.helloWord();
+    c.helloWord();
+  }
+
+  function pagarConta (tipo?: string) {
+    const pagamento: IPagamento = pagamentoFactory.factory(tipo);
+    console.log(pagamento.pagar());
+  }
+
   return (
     <div className="App">
       <h1>Olá Mundo</h1>
+      <button onClick={() => pagarConta('pix')}>pagamento</button>
     </div>
   );
 }
